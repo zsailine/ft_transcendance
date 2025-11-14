119366ae9016732de1636758e6f3dbc2b6b12cdc
zsailine <mickaeljoestar@gmail.com>
2025-11-14 16:02:30 +0300

    added

--- FRONTEND/src/Providers/OnlineGameProvider.tsx
+++ FRONTEND/src/Providers/OnlineGameProvider.tsx
@@ -1,9 +1,9 @@
-import { createContext } from "react-router-dom"
-import { useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 
 interface OnlineGameInterface {
 	text: string,
+	setText: (result: string) => void,
 }
 
 export const OnlineGameContext = createContext<OnlineGameInterface | null>(null);
@@ -13,4 +13,19 @@ export const useOnlineGame = () => {
 	if (!context)
 		throw new Error("Error in context");
 	return context
+}
+
+export const OnlineGameProvider = ({ children }: any) => {
+    const [text, setText] = useState<string>("");
+
+    const value = {
+        text,
+		setText
+    }
+
+    return (
+        <OnlineGameContext.Provider value={value}>
+            {children}
+        </OnlineGameContext.Provider>
+    )
 }
