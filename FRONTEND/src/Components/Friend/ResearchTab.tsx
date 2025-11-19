49093df6e1e49a5fed987295f5734952a298949e
mitiarenx <mitiasoafinaritra@gmail.com>
2025-11-19 09:51:03 +0300

    friend: tab switch implemented

--- FRONTEND/src/Components/Friend/ResearchTab.tsx
+++ FRONTEND/src/Components/Friend/ResearchTab.tsx
@@ -1,22 +1,35 @@
 import { useState, type FormEvent } from "react"
 import SearchBar from "../Chat/SearchBar"
 import type { UserInterface } from "../../Providers/ChatProvider";
+import { useFriend } from "../../Providers/FriendProvider";
+import FriendsList from "../Chat/FriendsList";
 
 function ResearchTab() {
 	const [ searchValue, setSearchValue ] = useState<string>("");
-	const	[ foundUser, setFoundUser ] = useState<UserInterface[]>([]);
+	const [ selectedUser, setSelectedUser ] = useState<UserInterface | null>(null);
+	const [ foundUser, setFoundUser ] = useState<UserInterface[]>([]);
+	const { notFriends } = useFriend();
 
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		setSearchValue(searchValue);
+		setFoundUser(await notFriends(searchValue));
 	}
 
   return (
-	<div>
+	<div className="flex flex-col gap-5">
 		<form onSubmit={handleSubmit}>
 			<SearchBar
 				searchValue={searchValue}
 				setSearchValue={setSearchValue}/>
 		</form>
+		{foundUser.length !== 0 ?
+			<FriendsList
+				friendsList={foundUser}
+				searchValue=""
+				setSelectedUser={setSelectedUser}
+				message={"research"}/>
+			: <></>}
 	</div>
   )
 }
