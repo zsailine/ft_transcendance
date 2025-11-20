daef8ad33cc573a8b1e24586e298fa200fe1611b
aranaivo <aranaivo@student.42antananarivo.mg>
2025-11-20 07:14:29 +0300

    fix scroll verticat and ajust sidebar

--- FRONTEND/src/Pages/Dashboard.tsx
+++ FRONTEND/src/Pages/Dashboard.tsx
@@ -4,11 +4,11 @@ import { Outlet } from 'react-router-dom';
 
 
 const Dashboard = () => {
-    return(
+    return (
         <>
-            <div className="bg-[#101728] min-h-screen flex">
-                <div className="w-max lg:w-1/6 ">
-                    <Sidebar/>
+            <div className="bg-[#101728] h-[100vh] flex">
+                <div className="sticky w-max lg:w-1/6 ">
+                    <Sidebar />
                 </div>
                 <div className="w-full md:w-4/5 lg:w-5/6 relative">
                     <div className="flex justify-end h-max mb-10">
