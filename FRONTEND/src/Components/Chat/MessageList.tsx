00652e794513276c0a47f59ba5fb37467f604841
zsailine <mickaeljoestar@gmail.com>
2025-12-17 15:09:18 +0300

    modified getHour

--- FRONTEND/src/Components/Chat/MessageList.tsx
+++ FRONTEND/src/Components/Chat/MessageList.tsx
@@ -7,21 +7,15 @@ const senderStyle = "self-end bg-cyan-500 text-white px-4 py-2 rounded-2xl round
 const receiverStyle = "self-start bg-gray-200/15 text-white px-4 py-2 rounded-2xl rounded-bl-none max-w-sm break-words shadow mb-3 transition-transform hover:-translate-y-0.5 flex flex-col gap-2";
 
 export const getHour = (timestamp: string) => {
-	const date = new Date(timestamp);
-	const newDate = date.toLocaleTimeString([], {
-		hour: '2-digit',
-		minute: '2-digit',
-		hour12: false
-	});
-	const tempDate = new Date();
-	const [ h, m ] = newDate.split(':').map(Number);
-	tempDate.setHours(h, m);
-	tempDate.setHours(tempDate.getHours() + 3);
-	return tempDate.toLocaleTimeString([], {
-		hour: '2-digit',
-		minute: '2-digit',
-		hour12: false
-	});
+    const date = new Date(timestamp);
+    date.setHours(date.getHours() + 3);
+    return date.toLocaleString('fr-FR', {
+        day: '2-digit',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    });
 }
 
 export function MessageList() {
